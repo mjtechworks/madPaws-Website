a7179d5393af4c1f7733978f193d5965833ec2e6
mjtechworks <michaeljohnson00518@gmail.com>
2020-10-29 11:53:09 +0100

    get list of user where role is petsitter

--- front/src/app/service/petsitters.service.ts
+++ front/src/app/service/petsitters.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { RestService } from './rest.service';
 import { HttpClient } from '@angular/common/http';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +9,6 @@ export class PetsittersService extends RestService {
 
   public constructor(protected http: HttpClient) {
     super(http);
-    this.setBaseUrl('/api/petsitter.php');
+    this.setBaseUrl('/api/petsitters.php');
   }
 }
