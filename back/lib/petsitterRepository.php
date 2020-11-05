f4299261e835dc162e5ff2c089c370c38bbb36b8
SuperDev0417 <happinessman0417@gmail.com>
2020-11-05 19:03:55 +0100

    send param between component, display results and design home

--- back/lib/petsitterRepository.php
+++ back/lib/petsitterRepository.php
@@ -5,25 +5,38 @@ require_once 'database.php';
 class PetsitterRepository
 {
 
-  public function search(string $type, string $service)
+  public function search($type, $service)
   {
     $sql = 
     ' SELECT *
       FROM user
       INNER JOIN petsitter_pet 
-      ON user.id = petsitter_pet.user_id
-      AND petsitter_pet.type = :petsitter_pet;
-      AND user.role = "petsitter"
+      ON user.id = petsitter_pet.user_id';
+
+    if ($type != null) {
+      $sql .= ' AND petsitter_pet.type = :petsitter_pet';
+    }
+      
+    $sql .= ' AND user.role = "petsitter"
 	    INNER JOIN petsitter_service
       ON user.id = petsitter_service.user_id
       INNER JOIN service
-      ON petsitter_service.service_id = service.id
-      AND service.label = :petsitter_service   
-    ';
+      ON petsitter_service.service_id = service.id';
+
+    if ($service != null) {
+      $sql .='  AND service.label = :petsitter_service'; 
+    }
 
     $stmt = Database::getInstance()->getConnection()->prepare($sql);
-    $stmt->bindParam(':petsitter_pet', $type, PDO::PARAM_STR);
-    $stmt->bindParam(':petsitter_service', $service, PDO::PARAM_STR);
+
+    if ($type != null) {
+      $stmt->bindParam(':petsitter_pet', $type, PDO::PARAM_STR);
+    }
+
+    if ($service != null) {
+      $stmt->bindParam(':petsitter_service', $service, PDO::PARAM_STR);
+    }
+
     $stmt->execute();
 
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
--- front/src/app/landing/landing.page.ts
+++ front/src/app/landing/landing.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { SearchToolbarComponent } from '../components/search-toolbar/search-toolbar.component';
+import { PetsittersService } from '../service/petsitters.service';
 
 @Component({
   selector: 'app-landing',
@@ -6,7 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['landing.page.scss']
 })
 export class LandingPage {
+  @ViewChild(SearchToolbarComponent) searchToolbar;
 
-  constructor() {}
+  public users = [];
 
+  public title =  'We care for your pet';
+
+  constructor(public petsittersService: PetsittersService) {}
+
+  public findSearchResults() {
+    this.petsittersService.getAll(this.searchToolbar.queryParams).then((data: any) => {
+      this.users = data;
+      console.warn('data', this.users, '===RESULTS===', this.searchToolbar.queryParams);
+    });
+  }
 }
